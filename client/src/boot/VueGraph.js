eda6f817439d1a4ce82b12607a64c763a48caa0a
Aljay Fagaragan <aljayf@hawaii.edu>
2019-11-23 12:01:16 -1000

    graph component setup

---
+++ client/src/boot/VueGraph.js
@@ -0,0 +1,9 @@
+// import something here
+import Vue from 'vue'
+import VueGraph from 'vue-graph'
+
+Vue.use(VueGraph);
+// // "async" is optional
+// export default async ({ /* app, router, Vue, ... */ }) => {
+//   // something to do
+// }
